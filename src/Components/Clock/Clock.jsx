ed03bf93b96ca32d37395800bd8fb786f20bab6c
me-mystic <Shrutikolla.2002@gmail.com>
2023-12-28 19:24:35 +0530

    feat: add clock functionality

--- src/Components/Clock/Clock.jsx
+++ src/Components/Clock/Clock.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import clockCss from './clock.module.css'
 
 const Clock = () => {
+    const dateobj = new Date();
+    const [time, setTime] = useState({ hours: dateobj.getHours(), minutes: dateobj.getMinutes(), seconds: dateobj.getSeconds() })
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const date = new Date();
+            setTime({
+                minutes: date.getMinutes(),
+                hours: date.getHours(),
+                seconds: date.getSeconds()
+            })
+        }, 1000)
+
+        return () => clearInterval(intervalId);
+    }, [])
+
     return (
         <div className={clockCss.clock_bg}>
             <div className={clockCss.container}>
@@ -9,8 +25,8 @@ const Clock = () => {
                     HOURS
                 </p>
                 <div className={clockCss.box}>
-                    <div className={clockCss.digit}>1</div>
-                    <div className={clockCss.digit}>7</div>
+                    <div className={clockCss.digit}>{Math.trunc(time.hours / 10)}</div>
+                    <div className={clockCss.digit}>{time.hours % 10}</div>
                 </div>
             </div> <span className={`${clockCss.separator} ${clockCss.gray}`}><strong>:</strong></span>
             <div className={clockCss.container}>
@@ -18,8 +34,8 @@ const Clock = () => {
                     MINUTES
                 </p>
                 <div className={clockCss.box}>
-                    <div className={clockCss.digit}>1</div>
-                    <div className={clockCss.digit}>7</div>
+                <div className={clockCss.digit}>{Math.trunc(time.minutes / 10)}</div>
+                    <div className={clockCss.digit}>{time.minutes % 10}</div>
                 </div>
             </div> <span className={`${clockCss.separator} ${clockCss.gray}`}><strong>:</strong></span>
             <div className={clockCss.container}>
@@ -27,8 +43,8 @@ const Clock = () => {
                     SECONDS
                 </p>
                 <div className={clockCss.box}>
-                    <div className={clockCss.digit}>1</div>
-                    <div className={clockCss.digit}>7</div>
+                <div className={clockCss.digit}>{Math.trunc(time.seconds / 10)}</div>
+                    <div className={clockCss.digit}>{time.seconds % 10}</div>
                 </div>
             </div>
         </div>
